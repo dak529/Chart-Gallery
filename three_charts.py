49262a7f6a079c0db51fdf8d17b3f4c80936528e
dak529 <dak529@stern.nyu.edu>
2020-06-04 20:42:21 -0400

    Update with Pie Chart and Dynamic Bar Chart

--- three_charts.py
+++ three_charts.py
@@ -14,6 +14,20 @@
 #print("GENERATING PIE CHART...")
 #print(pie_data) # TODO: create a pie chart based on the pie_data
 
+
+import plotly
+import plotly.graph_objs as go
+colors = ['gold', 'mediumturquoise', 'darkorange']
+fig = go.Figure(data=[go.Pie(
+            labels=['Company X','Company Y','Company Z'],
+            values=[0.55,0.30,0.15])])
+            #layout=(title="Pie Chart"))])
+#layout(title="Pie Chart")
+fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=20,
+                  marker=dict(colors=colors, line=dict(color='#000000', width=2)))
+fig.show()
+
+
 #
 # CHART 2 (LINE)
 #
@@ -53,14 +67,27 @@ print(bar_data) # TODO: create a horizontal bar chart based on the bar_data
 
 import plotly.graph_objects as go
 
+# Hard Coded (Trial 1)
+#fig = go.Figure(go.Bar(
+           # x=[123456, 234567, 987654, 876543, 283105, 544099, 121212],
+            #y=['Thriller', 'Mystery', 'Sci-Fi', 'Fantasy', 'Documentary','Action', 'Romantic Comedy'],
+            #orientation='h'))
 
-fig = go.Figure(go.Bar(
-            x=[123456, 234567, 987654, 876543, 283105, 544099, 121212],
-            y=['Thriller', 'Mystery', 'Sci-Fi', 'Fantasy', 'Documentary','Action', 'Romantic Comedy'],
-            orientation='h'))
+#fig.show()
 
+#Dynamic List pull Trial
+
+x = []
+y = []
+
+for d in bar_data:
+    x.append(d["viewers"])
+    y.append(d["genre"])
+
+fig = go.Figure([go.Bar(x=x, y=y, orientation='h')])
 fig.show()
 
+#Hard Coded (Example Online)
 
 #fig = go.Figure(go.Bar(
            #x=[20, 14, 23],
